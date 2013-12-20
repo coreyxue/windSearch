ae9fee76e46022d2e03050d24f000869da385647
coreyxue <coreyxueatcal@gmail.com>
2013-12-20 23:18:11 +0100

    factor result format

--- app/controllers/search_place_controller.rb
+++ app/controllers/search_place_controller.rb
@@ -7,29 +7,34 @@ class SearchPlaceController < ApplicationController
   	#1/0 #use to see the params in error page
   	# not allow search only for one charactor
   	if params[:name].length<2
-  		return @result = "no match found!"
+  		return @result = "Input more charactor!"
   	end
+
   	search_term = params[:name]
-  	@result=""
+  	@province_result=""
+  	@city_result=""
+  	@county_result=""
 
   	#prioritize search
   	if search_term[search_term.length-1]=='省'
   		province_info = Province.info(search_term)
-  		@result = province_info
+  		@province_result = province_info
   	elsif search_term[search_term.length-1]=='市'
   		city_info = City.info(search_term)
-  		@result = city_info.to_s
+  		@city_result = city_info.to_s
   	elsif search_term[search_term.length-1]=~/乡|县|村|区/
   		county_info = County.info(search_term)
-  		@result = county_info.to_s
+  		@county_result = county_info.to_s
   	end
 
   	#enhence search
-  	if @result == ""
+  	if @province_result == "" and @city_result == "" and @county_result == ""
 	   	province_info = Province.info(search_term)
 	  	city_info = City.info(search_term)
 	  	county_info = County.info(search_term)
-	  	@result = province_info.to_s+ city_info.to_s+ county_info.to_s
+	  	@province_result = province_info.to_s
+	  	@city_result = city_info.to_s
+	  	@county_result = county_info.to_s
 	end
   end
 end
